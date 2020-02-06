ffce19bf96e4add3b45628989398f8df13965b5e
teoboley <teo751@gmail.com>
2020-02-06 01:09:59 -0500

    Added new name and icon

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,6 +2,8 @@ import { app, BrowserWindow } from 'electron';
 
 declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
 
+const isDevelopment: boolean = process.env.NODE_ENV?.toLowerCase() === "development" || false;
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   app.quit();
@@ -23,7 +25,9 @@ const createWindow = (): void => {
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools({ mode: 'undocked' });
+  if (isDevelopment) {
+    mainWindow.webContents.openDevTools({ mode: 'undocked' });
+  }
 };
 
 // This method will be called when Electron has finished
